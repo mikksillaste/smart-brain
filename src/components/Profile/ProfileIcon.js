9a3305929795104ebe65d171045757f753514e00
mikksillaste <mikk.sillaste@gmail.com>
2018-11-14 22:24:24 +0200

    Profile icon

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -29,10 +29,11 @@ class ProfileIcon extends React.Component {
                             className="br-100 ba h3 w3 dib" alt="avatar" />
                     </DropdownToggle>
                     <DropdownMenu
+                        right
                         className="b--transparent shadow-5"
                         style={{marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
                             <DropdownItem>View Profile</DropdownItem>
-                            <DropdownItem onClick={this.props.onRouteChange('signout')}>Sign Out</DropdownItem>
+                            <DropdownItem onClick={() => this.props.onRouteChange('signout')}>Sign Out</DropdownItem>
                     </DropdownMenu>
                 </Dropdown>
             </div>
